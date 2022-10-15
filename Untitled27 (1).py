68136d59a1a24c94517343c6f1ff9a1dd2310356
rakeshpanda656 <115250976+rakeshpanda656@users.noreply.github.com>
2022-10-15 23:48:38 +0530

    Add files via upload

---
+++ Untitled27 (1).py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#sort
+import numpy as np
+arr1 = np.array([[[7,3],[2,9]],[[5,8],[1,4]]])
+print(np.sort(arr1))
+
+
+# In[ ]:
+
+
+#where
+import numpy as np
+arr2 = np.array([[[7,3],[2,9]],[[5,8],[1,4]]])
+print(np.where(arr2))
+
